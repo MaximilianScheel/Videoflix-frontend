7991f2172503b7520e0a9516f3e117674c2c8b31
Maximilian Scheel <scheel.development@gmail.com>
2024-03-17 17:24:33 +0100

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AllTodosComponent } from './components/all-todos/all-todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LoginComponent,
+    AllTodosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
